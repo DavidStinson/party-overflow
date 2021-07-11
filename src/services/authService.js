ea2ab7403e3ae6db434e520550d44334274ddec6
whlong1 <whunterlong@gmail.com>
2021-07-11 14:06:43 -0400

    recreate auth and token services

---
+++ src/services/authService.js
@@ -0,0 +1,49 @@
+import tokenService from './tokenService'
+const BASE_URL = '/api/users/'
+
+export function getUser() {
+    return tokenService.getUserFromToken()
+}
+
+export function getTopUsers() {
+    return fetch(BASE_URL, { mode: "cors" })
+        .then(res => res.json())
+}
+
+export function signup(user) {
+    return fetch(`${BASE_URL}signup`, {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        body: JSON.stringify(user)
+    })
+        .then(res => {
+            console.log(res, '<-- response object')
+            return res.json();
+        })
+        .then(json => {
+            if (json.token) return json;
+            console.log(json, '<-- the error')
+            throw new Error(`${json.err}`)
+        })
+        .then(({ token }) => {
+            tokenService.setToken(token);
+        })
+}
+
+export function login(creds) {
+    return fetch(`${BASE_URL}login`, {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        body: JSON.stringify(creds)
+    })
+        .then(res => {
+            if (res.ok) return res.json()
+            throw new Error('Bad Credentials!')
+        })
+        .then(({ token }) => tokenService.setToken(token))
+}
+
+export function logout() {
+    tokenService.removeToken()
+}
+
