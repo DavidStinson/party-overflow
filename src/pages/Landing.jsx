1167d47da19edb7ec1af7c389eac2588856872ec
whlong1 <whunterlong@gmail.com>
2021-07-18 23:12:44 -0400

    add preliminary styling to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import '../styles/Landing.css'
 
 const Landing = (props) => {
 
 
     return (
-        <div>
-            Landing
+        <div className="landing-page">
+            <h3>Welcome to</h3>
+            <h1>partyoverflow</h1>
+            <p>Lorem Ipsum</p>
+            <div>
+                <button>Sign Up</button>
+                <button>Sign In</button>
+            </div>
         </div>
     )
 }
