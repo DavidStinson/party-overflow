36faf84fd55133af37feb1617c689f64555f9958
whlong1 <whunterlong@gmail.com>
2021-07-23 17:16:26 -0400

    finish styling avatar selection pop up

--- src/components/misc/AvatarSelection.jsx
+++ src/components/misc/AvatarSelection.jsx
@@ -6,21 +6,26 @@ const AvatarSelection = (props) => {
 
     return (
         <div className="popup">
-            <div className="select-menu">
-                <button onClick={props.handlePopup}>X</button>
+            <div className="popup-menu">
+                <div className="popup-header">
+                    <h3>Select Your Avatar</h3>
+                    <button id="close-button" onClick={props.handlePopup}>X</button>
+                </div>
                 <img src={props.formData.avatar} alt="animal-avatar"></img>
-                <select onChange={(e) => props.handleChange(e)} name="avatar" value={props.formData.avatar}>
-                    <option value='https://i.imgur.com/Wdyo4ow.png'>Cat</option>
-                    <option value='https://i.imgur.com/74imy42.png'>Bear</option>
-                    <option value='https://i.imgur.com/51nVPDR.png'>Monkey</option>
-                    <option value='https://i.imgur.com/JjgmvrX.png'>Koala</option>
-                    <option value='https://i.imgur.com/qWHIXp5.png'>Fox</option>
-                    <option value='https://i.imgur.com/BC8wCCP.png'>Dear</option>
-                    <option value='https://i.imgur.com/ydToVuJ.png'>Raccoon</option>
-                    <option value='https://i.imgur.com/ut1szAk.png'>Panda</option>
-                    <option value='https://i.imgur.com/MULaROr.png'>Wolf</option>
-                </select>
-                <button onClick={props.handlePopup}>Confirm</button>
+                <div className="bottom-ui">
+                    <select onChange={(e) => props.handleChange(e)} name="avatar" value={props.formData.avatar}>
+                        <option value='https://i.imgur.com/Wdyo4ow.png'>Cat</option>
+                        <option value='https://i.imgur.com/74imy42.png'>Bear</option>
+                        <option value='https://i.imgur.com/51nVPDR.png'>Monkey</option>
+                        <option value='https://i.imgur.com/JjgmvrX.png'>Koala</option>
+                        <option value='https://i.imgur.com/qWHIXp5.png'>Fox</option>
+                        <option value='https://i.imgur.com/BC8wCCP.png'>Dear</option>
+                        <option value='https://i.imgur.com/ydToVuJ.png'>Raccoon</option>
+                        <option value='https://i.imgur.com/ut1szAk.png'>Panda</option>
+                        <option value='https://i.imgur.com/MULaROr.png'>Wolf</option>
+                    </select>
+                    <button onClick={props.handlePopup}>Confirm</button>
+                </div>
             </div>
         </div>
     )
