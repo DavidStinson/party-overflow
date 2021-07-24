500542577be40cb598bd83e282e46cf54ce5f83c
whlong1 <whunterlong@gmail.com>
2021-07-24 16:32:31 -0400

    add toggle close to mobile nav

--- src/components/misc/NavBar.jsx
+++ src/components/misc/NavBar.jsx
@@ -6,7 +6,7 @@ import '../../styles/NavBar.css'
 import SearchBar from './SearchBar'
 
 const NavBar = (props) => {
-    const { authenticated, handleLogout } = props
+    // const { authenticated, handleLogout } = props
 
     const navRef = useRef()
 
@@ -19,11 +19,11 @@ const NavBar = (props) => {
     return (
         <div className="nav-bar">
 
-            <Link className="logo" to='/'>
+            <Link className="logo" to='/home'>
                 <img src="https://i.imgur.com/Vba6pJv.png" alt="disco-ball"></img>
             </Link>
 
-            <Link to='/home'>Home</Link>
+            {/* <Link to='/home'>Home</Link>
             {authenticated ?
                 <p onClick={handleLogout}>Sign Out</p>
                 :
@@ -31,7 +31,7 @@ const NavBar = (props) => {
                     <Link to='/login'>Login</Link>
                     <Link to='/register'>Register</Link>
                 </div>
-            }
+            } */}
             <SearchBar
                 setPosts={props.setPosts}
                 setDisplay={props.setDisplay}
@@ -42,10 +42,15 @@ const NavBar = (props) => {
                 <img src="https://i.imgur.com/e2wfqY1.png" alt="hamburger icon"></img>
             </button>
 
+
             <div className="mobile-nav" ref={navRef}>
-
-
-
+                <button className="hamburger" onClick={handleClick}>
+                    <img
+                        style={{ width: '20px', padding: '10px' }}
+                        src="https://i.imgur.com/wpgp3TT.png"
+                        alt="hamburger icon"
+                    ></img>
+                </button>
             </div>
 
         </div>
