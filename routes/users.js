689d0ab31b10bfe72388f1cf74b6bf4b1cf9e2b5
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-07-27 17:29:19 -0500

    build users router

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import * as usersCtrl from '../controllers/users.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+router.get('/top-users', usersCtrl.getTopUsers)
+
+// ========= Protected Routes ========= 
+
+
+export {
+   router 
+}
