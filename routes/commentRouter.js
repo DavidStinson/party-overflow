e443f62345dd5d305169585cf0a2a35bb1a206eb
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-07-27 10:29:20 -0500

    make use of decodeUserFromToken middleware

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import * as commentCtrl from '../controllers/comment.js'
+import { decodeUserFromToken } from '../middleware/auth.js'
+
 
 const router = Router()
 
 // ========= Public Routes ========= 
 
 // ========= Protected Routes ========= 
-router.use(require('../config/auth'))
+router.use(decodeUserFromToken)
 router.post('/:post_id', checkAuth, commentCtrl.createComment)
 router.delete('/:post_id/:comment_id', checkAuth, commentCtrl.deleteComment)
 router.put('/:comment_id/:post_id/:user_id', checkAuth, commentCtrl.updateComment)
