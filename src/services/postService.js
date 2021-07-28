bb76c4e109507240482060221fc5dd3600c14ace
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-07-28 09:16:57 -0500

    move comment service into post service

--- src/services/postService.js
+++ src/services/postService.js
@@ -100,3 +100,49 @@ export const deletePost = async (postId) => {
         throw error
     }
 }
+
+export const createComment = async (postId, comment) => {
+    try {
+        const res = await fetch(`${BASE_URL}${postId}`, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${tokenService.getToken()}`
+            },
+            body: JSON.stringify(comment)
+        }, { mode: "cors" })
+        const data = await res.json()
+        return data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const updateComment = async (commentId, postId) => {
+    try {
+        const res = await fetch(`${BASE_URL}${commentId}/${postId}`, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': 'Bearer ' + tokenService.getToken()
+            },
+        }, { mode: "cors" })
+        const data = await res.json()
+        return data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const deleteComment = async (postId, commentId) => {
+    try {
+        const res = await fetch(`${BASE_URL}${postId}/${commentId}`, {
+            method: 'DELETE',
+            headers: { 'Authorization': 'Bearer ' + tokenService.getToken() }
+        }, { mode: "cors" })
+        const data = await res.json()
+        return data
+    } catch (error) {
+        throw error
+    }
+}
