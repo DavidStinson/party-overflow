5d832e47c88ddc181cb3d7d27d6c4bbdb7510b5f
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-07-28 08:51:51 -0500

    move comments into posts controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -92,6 +92,51 @@ const deletePost = async (req, res) => {
     }
 }
 
+const createComment = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id)
+        post.comments.push(req.body)
+        await post.save()
+        const newComment = post.comments[post.comments.length - 1]
+        res.json(newComment)
+    } catch (err) {
+        res.status(400).send(err.message)
+    }
+}
+
+const deleteComment = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id)
+        const idx = post.comments.findIndex((comment) =>
+            comment._id.equals(req.params.comment_id)
+        )
+        const removedComment = post.comments.splice(idx, 1)
+        await post.save()
+        res.json(removedComment)
+    } catch (err) {
+        res.json(err)
+    }
+}
+
+const updateComment = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id)
+        const idx = post.comments.findIndex((comment) =>
+            comment._id.equals(req.params.comment_id)
+        )
+        post.is_resolved = true
+        post.comments[idx].is_solution = true
+        await User.updateOne(
+            { _id: req.user._id },
+            { $inc: { solution_count: 1 } }
+        )
+        await post.save()
+        res.json(post)
+    } catch (err) {
+        res.json(err)
+    }
+}
+
 export {
     createPost,
     getRecent,
@@ -100,4 +145,7 @@ export {
     getPostsByUserId,
     searchPosts,
     getPostById,
+    createComment,
+    deleteComment,
+    updateComment,
 }
