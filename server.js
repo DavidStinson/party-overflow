a07c937ce1d0c08fc2ca839c1990353eb422e568
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-07-28 09:15:29 -0500

    remove comments router

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ import cors from 'cors'
 
 import { router as authRouter } from './routes/auth.js'
 import { router as postsRouter } from './routes/posts.js'
-import { router as commentsRouter } from './routes/comments.js'
 import { router as usersRouter } from './routes/users.js'
 
 const app = express()
@@ -20,7 +19,6 @@ app.use(express.json())
 
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postsRouter)
-app.use('/api/comments', commentsRouter)
 app.use('/api/users', usersRouter)
 
 app.get('/*', (req, res) => {
