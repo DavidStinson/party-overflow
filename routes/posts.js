5bb89a65d40fc66389f0e0f037464d2b68baddea
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-07-27 19:08:11 -0500

    cleanup routers

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import * as postCtrl from '../controllers/posts.js'
-import { decodeUserFromToken } from '../middleware/auth.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
 
-// ========= Public Routes ========= 
+// ========== Public Routes ===========
 
 router.get('/:page', postCtrl.getRecent)
 router.get('/questions/search', postCtrl.searchPosts)
@@ -17,11 +17,6 @@ router.post('/', checkAuth, postCtrl.createPost)
 router.put('/:id', checkAuth, postCtrl.updatePost)
 router.delete('/:user_id/:post_id', checkAuth, postCtrl.deletePost)
 router.get('/user/:user_id', checkAuth, postCtrl.getPostsByUserId)
-
-
-function checkAuth(req, res, next) {
-    return req.user ? next() : res.status(401).json({ msg: 'Not Authorized' })
-}
 
 export {
     router
